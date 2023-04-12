f0959cbb80a9c1fceefffb6876cdb38e2f4de59d
Maxence Dassaud <max.dassaud@hotmail.fr>
2023-04-12 17:28:54 +0200

    frontcart

---
+++ cart.js
@@ -0,0 +1,36 @@
+const cartCont = document.querySelector('#cart-container')
+
+fetch("http://localhost:3000/cart")
+.then(response => response.json())
+.then(data => {
+    if (data.data.length>0){
+        let inner = "";
+        let total = 0
+        for (let items of data.data) {
+            total += Number(items.price)
+            inner += `
+              <div class="trips">
+                <p id="depart">${items.departure}</p>
+                <p> > </p>
+                <p id="arrival">${items.arrival}</p>
+                <p id="hour">${items.date}</p>
+                <div id= "price-div">
+                <p id="price">${items.price}</p> 
+                <p>  €</p>
+                </div>
+                <button class="btn-booking" onclick="location.href = 'http://127.0.0.1:5500/frontend-hackaton/cart.html'" id="btn-delete" onclick="" type="button">X</button>
+              </div>
+            `;
+            
+          }
+          cartCont.innerHTML = 
+          "<p>My cart</p>"+
+          inner +
+          `<div id ="total">
+          <p>Total: ${total} €</p>
+          <button class="btn-booking" onclick="location.href = 'http://127.0.0.1:5500/frontend-hackaton/booking.html'" id="btn-book" onclick="" type="button">Purchase</button>
+          </div>`
+          cartCont.style.overflow = "auto"
+        }
+
+})
--- script.js
+++ script.js
@@ -1,65 +1,144 @@
-// const { get } = require("mongoose");
-const departureInput = document.querySelector ("#add-departure")
-const arrivalInput = document.querySelector ("#add-arrival")
-const searchBtn = document.querySelector("#btn-search")
-const dateInput = document.querySelector ("#calendar")
+const departureInput = document.querySelector("#add-departure");
+const arrivalInput = document.querySelector("#add-arrival");
+const searchBtn = document.querySelector("#btn-search");
+const dateInput = document.querySelector("#calendar");
 const trainImage = document.querySelector("#img-train");
-const noTripFound = document.querySelector("#booking-choice h3")
-const tripDetails = `${departureInput} > ${arrivalInput}`
+const noTripFound = document.querySelector("#booking-choice h3");
 const cartContainer = document.querySelector("#booking-choice");
 
+searchBtn.addEventListener("click", function () {
+  const departureValue = departureInput.value;
+  const arrivalValue = arrivalInput.value;
+  const dateValue = dateInput.value;
 
-searchBtn.addEventListener("click" , function(){
-    const departureValue = departureInput.value
-    const arrivalValue = arrivalInput.value 
-    const dateValue = dateInput.value
-
-       
-   if (departureValue.trim() !== "" && arrivalValue.trim() !== "") {
-    
+  if (departureValue.trim() !== "" && arrivalValue.trim() !== "") {
     console.log("Départ:", departureValue);
     console.log("Arrivée:", arrivalValue);
     console.log("date", dateValue);
 
-
-    fetch ('http://localhost:3000/trips',
-    {method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({departure : departureValue,
-    arrival : arrivalValue,
-    date: dateValue}),
-    }
-    )
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let inner = ""
-        for (let items of data.newTrips) {
-            inner += 
-            `<div class="trips">
-            <p>${items.departure} > ${items.arrival}</p>
-            <p>${items.date}</p>
-            <p>${items.price} €</p>
-            <button id="btn-book" type="button">Book</button>
-        </div>`}
-
-        cartContainer.innerHTML = inner
-        cartContainer.style.overflow = "auto"
-        // cartContainer.id = "#booking-choice2"
+    fetch("http://localhost:3000/trips", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        departure: departureValue,
+        arrival: arrivalValue,
+        date: dateValue,
+      }),
     })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.newTrips)
+        if (data.newTrips.length>0){
+        let inner = "";
+        for (let items of data.newTrips) {
+          
+            inner += `
+              <div class="trips">
+                <p id="depart">${items.departure}</p>
+                <p> > </p>
+                <p id="arrival">${items.arrival}</p>
+                <p id="hour">${items.date}</p>
+                <p id="price">${items.price}</p> €
+                <button class="btn-booking" onclick="location.href = 'http://127.0.0.1:5500/frontend-hackaton/cart.html'" id="btn-book" onclick="" type="button">Book</button>
+              </div>
+            `;
+            
+          }
+          cartContainer.innerHTML = inner
+          cartContainer.style.overflow = "auto"
+        }
+        else  {
+          trainImage.src = "images/notfound.png";
+          noTripFound.textContent = "No trip found";
+
+        } 
+        
+        const bookButton = document.querySelectorAll('.btn-booking')
+        for (let items of bookButton) {
+            items.addEventListener('click', function () {
+                
+                fetch('http://localhost:3000/cart',{
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                      departure: this.parentNode.querySelector('#depart').textContent,
+                      arrival: this.parentNode.querySelector('#arrival').textContent,
+                      date: this.parentNode.querySelector('#hour').textContent,
+                      price: this.parentNode.querySelector('#price').textContent
+                    }),
+                  }
+                ).then(data => console.log(data))
+            })
+
+        }
+     }) }})
+      
+
+
+
+   
+         
+
+
+
+// // const { get } = require("mongoose");
+// const departureInput = document.querySelector ("#add-departure")
+// const arrivalInput = document.querySelector ("#add-arrival")
+// const searchBtn = document.querySelector("#btn-search")
+// const dateInput = document.querySelector ("#calendar")
+// const trainImage = document.querySelector("#img-train");
+// const noTripFound = document.querySelector("#booking-choice h3")
+// const tripDetails = `${departureInput} > ${arrivalInput}`
+// const cartContainer = document.querySelector("#booking-choice");
+
+
+// searchBtn.addEventListener("click" , function(){
+//     const departureValue = departureInput.value
+//     const arrivalValue = arrivalInput.value 
+//     const dateValue = dateInput.value
+
+       
+//    if (departureValue.trim() !== "" && arrivalValue.trim() !== "") {
+    
+//     console.log("Départ:", departureValue);
+//     console.log("Arrivée:", arrivalValue);
+//     console.log("date", dateValue);
+
+
+//     fetch ('http://localhost:3000/trips',
+//     {method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({departure : departureValue,
+//     arrival : arrivalValue,
+//     date: dateValue}),
+//     }
+//     )
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         let inner = ""
+//         for (let items of data.newTrips) {
+//             inner += 
+//             `<div class="trips">
+//             <p>${items.departure} > ${items.arrival}</p>
+//             <p>${items.date}</p>
+//             <p>${items.price} €</p>
+//             <button id="btn-book" type="button">Book</button>
+//         </div>`}
+
+//         cartContainer.innerHTML = inner
+//         cartContainer.style.overflow = "auto"
+//         // cartContainer.id = "#booking-choice2"
+//     })
 
 
     
    
-    // window.location.href = "test.html";
-} else {
+//     // window.location.href = "test.html";
+// } else {
 
-    alert("Please fill in all the fields.");
-    trainImage.src = "images/notfound.png";
-    noTripFound.textContent = "No trip yet";
-}
-});
-
-
-
-
+//     alert("Please fill in all the fields.");
+//     trainImage.src = "images/notfound.png";
+//     noTripFound.textContent = "No trip yet";
+// }
+// });
