ab6ee2f7a09dd9297e19d3a7973d37f94ba4801e
Maxence Dassaud <max.dassaud@hotmail.fr>
2023-04-12 11:51:13 +0200

    test

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// const { get } = require("mongoose");
 const departureInput = document.querySelector ("#add-departure")
 const arrivalInput = document.querySelector ("#add-arrival")
 const searchBtn = document.querySelector("#btn-search")
 const dateInput = document.querySelector ("#calendar")
 const trainImage = document.querySelector("#img-train");
 const noTripFound = document.querySelector("#booking-choice h3")
+const tripDetails = `${departureInput} > ${arrivalInput}`
+const cartContainer = document.querySelector("#booking-choice");
+
 
 searchBtn.addEventListener("click" , function(){
     const departureValue = departureInput.value
@@ -16,6 +20,36 @@ searchBtn.addEventListener("click" , function(){
     console.log("Départ:", departureValue);
     console.log("Arrivée:", arrivalValue);
     console.log("date", dateValue);
+
+    let filter = {
+        departure : departureValue,
+        arrival: arrivalValue,
+        date: dateValue
+    }
+
+    fetch ('http://localhost:3000/trips',
+    {method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({filter}),
+    }
+    )
+    .then(response => response.json())
+    .then(data => {
+    
+    const trips = data.data.filter(e => e.departure.toLowerCase() == departureValue.toLowerCase() && e.arrival.toLowerCase() == arrivalValue.toLowerCase() 
+             && new Date(e.date).getDay() == new Date(dateValue).getDay() &&
+              )
+         console.log(new Date(data.data[0].date).getDay())
+        console.log(new Date(dateValue).getDay())
+        console.log(new Date(data.data[0].date).getMonth())
+        console.log(new Date(dateValue).getMonth())
+        // console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+            
+          console.log(data.data[0])
+    })
+
+
     
    
     // window.location.href = "test.html";
